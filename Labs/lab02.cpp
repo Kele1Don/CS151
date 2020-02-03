1a71d7a322e66de80fdf9e8beb5eb01fb5e83f7a
Kele1Don <kelia.morrah@student.mec.cuny.edu>
2020-02-03 10:22:40 -0500

    Push

---
+++ Labs/lab02.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    cout << "Image I\n";
+    /*Image I Begins*/
+    cout <<"*************************\n" <<"*************************\n" <<"*************************\n" <<"*************************\n" <<"*************************\n";
+    /*Image I Ends*/
+
+    cout << "\nImage II\n";
+    /*Image II Begins*/
+    cout <<"********  ********\n" <<"********  ********\n" <<"   **        **\n" <<"   **        **\n" <<"   **        **\n" <<"   **        **\n";
+    cout <<"********     **\n" <<"********     **\n" ;
+    /*Image II Ends*/
+
+    cout << "\nImage III\n";
+    /*Image III Begins*/
+    cout <<"      **\n" <<"     ****\n" <<"    ******\n" <<"   ********\n" <<"  **********\n" <<" ************\n" <<"**************\n";
+    /*Image III Ends*/
+
+    return 0;
+}
