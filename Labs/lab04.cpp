0f64f8c7f2920ea49d0a2c36c2b296930008208b
Kele1Don <kelia.morrah@student.mec.cuny.edu>
2020-02-10 09:02:22 -0500

    Eh

---
+++ Labs/lab04.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+    string fname;
+    string lname;
+    fname= "Kelia";
+    lname= "Morrah";
+    cout << "************" <<end1;
+    cout << lname << ",";
+    cout << fname <<end1;
+    cout << "************";
+    
+return 0;
+}
