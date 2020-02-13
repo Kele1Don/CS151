d6fe38de17714532bf4eaa3ee736bc493509b2da
Kele1Don <kelia.morrah@student.mec.cuny.edu>
2020-02-13 17:42:52 -0500

    Monday

---
+++ Projects/Planning.cpp
@@ -0,0 +1,11 @@
+/*The game chosen is the Wheel of Fortune which allows three (3) players, the most, to compete in puzzle-solving trivia determined by spinning a carnival wheel.
+The game is based on the television show created by Merv Griffin in 1975 which was derived from the game of Hangman.
+Every round of the game starts off with blank spaces and/or punctuation marks to represent a word or phrase.
+The Wheel consists of prizes which would be considered as the Fortune but also features the misfortune of going “Bankrupt” and “Losing a turn”.
+Each player gets a chance to spin the wheel to determine a prize value and to guess a consonant while trying to avoid the misfortunes.
+There are four (4) rounds in total to determine a winner if there isn’t a tie. 
+In the first three (3) rounds, the wheel contains a Wild Card. 
+The Wild Card may be used to call an additional consonant after any turn or taken to the bonus round to call an extra consonant there.
+Each round features five toss-up puzzles, which reveal the puzzle one random letter at a time, and awards to whoever answers with the right answer. 
+If a tie for first place occurs after the third round, an additional toss-up puzzle is played between the players who are tied for the win. 
+The player who wins then advances to the bonus round.*/
