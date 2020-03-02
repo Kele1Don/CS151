f48fb024efe64d688526eef1eb391ef88b53522a
Kele1Don <kelia.morrah@student.mec.cuny.edu>
2020-03-02 09:50:05 -0500

    Fin

---
+++ Labs/lab07.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+/*********************************************
+ * int main()
+ * {
+ *  double x1, x2, x3, x4, smv;
+ *  double d1, d2, d3, d4;
+ * 
+ *  cout << "Enter value for x1: ";
+ *  cin >> x1;
+ * 
+ *  cout << "Enter value for x2: ";
+ *  cin >> x2;
+ * 
+ *  cout << "Enter value for x3: ";
+ *  cin >> x3;
+ * 
+ *  cout << "Enter value for x4: ";
+ *  cin >> x4;
+ * 
+ *  smv = x1 + x2 + x3 + x4;
+ *  smv /= 4;
+ * 
+ *  d1 = smv - x1;
+ *  d2 = smv - x2;
+ *  d3 = smv - x3;
+ *  d4 = smv - x4;
+ * 
+ *  d1 = d1 * d1;
+ *  d2 = d2 * d2;
+ *  d3 = d3 * d3;
+ *  d4 = d4 * d4;
+ * 
+ *  smv = d1 + d2 + d3 + d4;
+ *  smv /= 4;
+ *  cout << "Result: " << smv << "\n";
+ * 
+ *  return 0;
+ *  cout << "All Done\n";
+ * }
+ **********************************************/
+
+int main()
+{
+    cout << "Trace Table\n\n";
+
+    cout << "1. output <- out\n";
+    cout << "2. x1 <- 2\n";
+    cout << "3. output <- out\n";
+    cout << "4. x2 <- -3\n";
+    cout << "5. output <- out\n";
+    cout << "6. x3 <- 6\n";
+    cout << "7. output <- out\n";
+    cout << "8. x4 <- 7\n";
+    cout << "9. smv <- 12\n";
+    cout << "10. smv <- 3\n";
+  
+    cout << "11. d1 <- 1\n";
+    cout << "12. d2 <- 6\n";
+    cout << "13. d3 <- -3\n";
+    cout << "14. d4 <- -4\n\n";
+    cout << "15. d1 <- 1\n";
+    cout << "16. d2 <- 36\n";
+    cout << "17. d3 <- 9\n";
+    cout << "18. d4 <- 16\n\n";
+    cout << "19. smv <- 62\n";
+    cout << "20. smv <- 15\n";
+    cout << "21. output <- out\n";
+    cout << "22. return <- 0\n\n";
+    cout << "23. All Done\n";
+    
+    cout << "out\n";
+    cout << "Enter value for x1: \n";
+    cout << "Enter value for x2: \n";
+    cout << "Enter value for x3: \n";
+    cout << "Enter value for x4: \n";
+    cout << "Result:15 \n";
+    cout << "All Done\n";
+    return 0;
+}
